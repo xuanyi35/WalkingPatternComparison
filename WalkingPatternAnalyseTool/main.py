4d3c962c8070233188e5724fadd7716366580611
xuanyi35 <31713273+xuanyi35@users.noreply.github.com>
2020-03-06 16:11:16 -0700

    add images in one window

--- WalkingPatternAnalyseTool/main.py
+++ WalkingPatternAnalyseTool/main.py
@@ -2,10 +2,52 @@ import sys
 from WalkingPatternAnalyseTool import PdVisualization
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+import os
+import sys
+
+
+from PyQt5 import Qt, QtCore
+start = 1
+
+class LeftWindow(Qt.QGraphicsView):
+    def __init__(self, viewWin, parent=None):
+        Qt.QGraphicsView.__init__(self, parent)
+        self.scene  = Qt.QGraphicsScene()
+        self.grview = viewWin
+        dir = "frames"
+        self.listFiles = os.listdir(dir)
+        self.timer = Qt.QTimer(self)
+        self.n = 0
+        self.timer.timeout.connect(self.on_timeout)
+        self.timer.start(100)
+        self.grview.show()
+
+    def on_timeout(self):
+        if start :
+            if self.n < len(self.listFiles):
+                self.scene.clear()
+                file = self.listFiles[self.n]
+                pixmap = Qt.QPixmap("frames\{}".format(file))    # !!!  "frames\{}"
+                self.scene.addPixmap(pixmap)
+                self.grview.fitInView(self.scene.itemsBoundingRect(), QtCore.Qt.KeepAspectRatio)
+
+                self.grview.setScene(self.scene)                 # !!! (self.scene)
+                self.n += 1
+            else:
+                self.timer.stop()
+
+
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
+
+
+
     ui = PdVisualization.Ui_mainWindow()
     ui.setupUi(MainWindow)
+    ui.graphicsView = LeftWindow( ui.graphicsView )
+
     MainWindow.show()
     sys.exit(app.exec_())
